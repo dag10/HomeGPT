177f09f2a1c7bdaf0c1b2e56fa4079be544db1e6
Drew Gottlieb <gottlieb.drew@gmail.com>
2023-01-29 18:57:21 -0800

    Load config.json and verify everything is present.

--- __main__.py
+++ __main__.py
@@ -1,9 +1,24 @@
 import requests
+import json
 
-url = "http://httpstat.us/200"
+def load_config_or_exit():
+    """Loads config.json."""
+    try:
+        config_file = open('config.json', 'r')
+        config_data = json.load(config_file)
+        config_file.close()
+        # Access some values to throw exceptions if not found
+        config_data['home_assistant_token']
+        config_data['openai_api_key']
+        return config_data
+    except FileNotFoundError:
+        print("Copy config.example.json to config.json and complete it.")
+        exit(1)
+    except Exception as e:
+        print("Invalid config.json: %s" % str(e))
+        exit(1)
 
 if __name__ == '__main__':
-    """Test request"""
-    response = requests.get(url)
-    print(response.text)
+    config = load_config_or_exit()
+    print(config)
 
